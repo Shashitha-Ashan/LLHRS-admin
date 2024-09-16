3e3d3a4752d9c15ef16a9416313eb1c94912adfe
Shashitha-Ashan <shashithaashan555@gmail.com>
2024-09-16 19:33:59 +0530

    add charts to home page

--- src/pages/home-page.tsx
+++ src/pages/home-page.tsx
@@ -1,7 +1,56 @@
+import { Typography } from "@mui/material";
+import { BarChart } from "@mui/x-charts/BarChart";
+import { PieChart } from "@mui/x-charts/PieChart";
+
 function Home() {
   return (
     <div>
-      <h1>Home</h1>
+      <Typography
+        variant="h5"
+        component="div"
+        sx={{
+          flexGrow: 1,
+          fontWeight: "bold",
+          textAlign: "center",
+          marginTop: "10px",
+        }}
+      >
+        Month Summary
+      </Typography>
+      <div style={{ display: "flex", alignItems: "center" }}>
+        <BarChart
+          xAxis={[
+            {
+              scaleType: "band",
+
+              data: ["1st Year", "2nd Year", "3rd Year", "4th Year"],
+            },
+            { label: "Years" },
+          ]}
+          series={[
+            { data: [4, 3, 5, 10] },
+            { data: [1, 6, 3, 12] },
+            { data: [2, 5, 6, 5] },
+          ]}
+          width={600}
+          height={500}
+          yAxis={[{ label: "" }]}
+        />
+        <PieChart
+          series={[
+            {
+              data: [
+                { id: 0, value: 10, label: "1st Year" },
+                { id: 1, value: 15, label: "2nd Year" },
+                { id: 2, value: 20, label: "3rd Year" },
+                { id: 2, value: 20, label: "4th Year" },
+              ],
+            },
+          ]}
+          width={400}
+          height={200}
+        />
+      </div>
     </div>
   );
 }
