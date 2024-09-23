91419fd2c7376964f4f7c340e54b96776e797e8c
Shashitha-Ashan <shashithaashan555@gmail.com>
2024-09-23 20:46:30 +0530

    add drawer navigation

--- src/layouts/nav-bar.tsx
+++ src/layouts/nav-bar.tsx
@@ -1,83 +1,134 @@
-import { Link } from "react-router-dom";
-import Constants from "../styles/styles";
+import { Link, useNavigate } from "react-router-dom";
 import AccountMenu from "../components/profile-options";
 import {
   Toolbar,
   Typography,
   IconButton,
   AppBar,
-  Tooltip,
+  Drawer,
+  Box,
+  Divider,
+  List,
+  ListItem,
+  ListItemIcon,
+  ListItemText,
+  ListItemButton,
 } from "@mui/material";
 import ViewModuleIcon from "@mui/icons-material/ViewModule";
 import CalendarMonthIcon from "@mui/icons-material/CalendarMonth";
 import GroupIcon from "@mui/icons-material/Group";
 import ApartmentIcon from "@mui/icons-material/Apartment";
 import BadgeIcon from "@mui/icons-material/Badge";
-import HistoryIcon from "@mui/icons-material/History";
+// import HistoryIcon from "@mui/icons-material/History";
+import MenuIcon from "@mui/icons-material/Menu";
+import { useState } from "react";
+import Home from "@mui/icons-material/Home";
 
 function NavBar() {
-  const linkStyle = {
-    textDecoration: "none",
-    color: "inherit",
+  const navigate = useNavigate();
+  const btnStyle = { mr: "20px" };
+
+  const [open, setOpen] = useState(false);
+
+  const toggleDrawer = (newOpen: boolean) => () => {
+    setOpen(newOpen);
   };
+  const DrawerList = (
+    <Box sx={{ width: 300 }} role="presentation" onClick={toggleDrawer(false)}>
+      <List>
+        <ListItem key="Home" disablePadding>
+          <ListItemButton onClick={() => navigate("/")}>
+            <ListItemIcon>
+              <Home sx={btnStyle} />
+              <ListItemText primary="Home" />
+            </ListItemIcon>
+          </ListItemButton>
+        </ListItem>
+        <Divider />
+        <ListItem key="TimeTable" disablePadding>
+          <ListItemButton onClick={() => navigate("/time-table")}>
+            <ListItemIcon>
+              <CalendarMonthIcon sx={btnStyle} />
+              <ListItemText primary="TimeTable" />
+            </ListItemIcon>
+          </ListItemButton>
+        </ListItem>
+        <Divider />
+        <ListItem key="Users" disablePadding>
+          <ListItemButton onClick={() => navigate("/users")}>
+            <ListItemIcon>
+              <GroupIcon sx={btnStyle} />
+              <ListItemText primary="Users" />
+            </ListItemIcon>
+          </ListItemButton>
+        </ListItem>
+        <Divider />
+        <ListItem key="Modules" disablePadding>
+          <ListItemButton onClick={() => navigate("/modules")}>
+            <ListItemIcon>
+              <ViewModuleIcon sx={btnStyle} />
+              <ListItemText primary="Modules" />
+            </ListItemIcon>
+          </ListItemButton>
+        </ListItem>
+        <Divider />
+        <ListItem key="Batches" disablePadding>
+          <ListItemButton onClick={() => navigate("/batches")}>
+            <ListItemIcon>
+              <BadgeIcon sx={btnStyle} />
+              <ListItemText primary="Batch Management Dashboard" />
+            </ListItemIcon>
+          </ListItemButton>
+        </ListItem>
+        <Divider />
+        <ListItem key="Halls" disablePadding>
+          <ListItemButton onClick={() => navigate("/halls")}>
+            <ListItemIcon>
+              <ApartmentIcon sx={btnStyle} />
+              <ListItemText primary="Halls" />
+            </ListItemIcon>
+          </ListItemButton>
+        </ListItem>
+        <Divider />
+        {/* <ListItem key="History" disablePadding>
+          <ListItemButton onClick={() => navigate("/history")}>
+            <ListItemIcon>
+              <HistoryIcon />
+              <ListItemText primary="History" />
+            </ListItemIcon>
+          </ListItemButton>
+        </ListItem>
+        <Divider /> */}
+      </List>
+    </Box>
+  );
+
   return (
     <AppBar
       position="static"
       sx={{ backgroundColor: "#4CAF50", margin: 0, padding: 0 }}
     >
       <Toolbar>
-        <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+        <IconButton color="inherit" onClick={toggleDrawer(true)}>
+          <MenuIcon sx={{ fontSize: 40 }} />
+        </IconButton>
+        <Typography
+          variant="h6"
+          component="div"
+          sx={{ flexGrow: 1, ml: "20px" }}
+        >
           <Link to={"/"} style={{ textDecoration: "none", color: "inherit" }}>
             Reserv
           </Link>
         </Typography>
-        <Tooltip title="TimeTable" arrow>
-          <Link style={linkStyle} to={"/time-table"}>
-            <IconButton color="inherit" sx={Constants.navIconStyle}>
-              <CalendarMonthIcon />
-            </IconButton>
-          </Link>
-        </Tooltip>
-
-        <Tooltip title="Users" arrow>
-          <Link to={"/users"} style={linkStyle}>
-            <IconButton color="inherit" sx={Constants.navIconStyle}>
-              <GroupIcon />
-            </IconButton>
-          </Link>
-        </Tooltip>
-
-        <Tooltip title="Modules" arrow>
-          <Link to={"/modules"} style={linkStyle}>
-            <IconButton color="inherit" sx={Constants.navIconStyle}>
-              <ViewModuleIcon />
-            </IconButton>
-          </Link>
-        </Tooltip>
-        <Tooltip title="Batch Management Dashboard" arrow>
-          <Link style={linkStyle} to={"/batches"}>
-            <IconButton color="inherit" sx={Constants.navIconStyle}>
-              <BadgeIcon />
-            </IconButton>
-          </Link>
-        </Tooltip>
-        <Tooltip title="Halls" arrow>
-          <Link style={linkStyle} to={"/halls"}>
-            <IconButton color="inherit" sx={Constants.navIconStyle}>
-              <ApartmentIcon />
-            </IconButton>
-          </Link>
-        </Tooltip>
-        <Tooltip title="History" arrow>
-          <Link style={linkStyle} to={"/halls"}>
-            <IconButton color="inherit" sx={Constants.navIconStyle}>
-              <HistoryIcon />
-            </IconButton>
-          </Link>
-        </Tooltip>
 
         <AccountMenu />
       </Toolbar>
+      <div>
+        <Drawer open={open} onClose={toggleDrawer(false)}>
+          {DrawerList}
+        </Drawer>
+      </div>
     </AppBar>
   );
 }
